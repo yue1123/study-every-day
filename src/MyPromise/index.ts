eaee67fd4e3f4ea97fab06ec87da69ebfe43aa55
花开半亩地 <2572468699@qq.com>
2022-01-25 18:14:29 +0800

    链式调用

--- src/MyPromise/index.ts
+++ src/MyPromise/index.ts
@@ -4,9 +4,11 @@ enum Status {
     REJECTED = 'rejected'
 }
 
-import { Executor, onFulfilled, onRejected } from './my-promise'
+import { Executor, onFulfilled, onRejected, Reject, Resolve } from './my-promise'
 
+// 判断是不是thenable对象
 function isPromise(value: any): value is PromiseLike<any> {
+    // 如果时不为null的对象或者function(函数也可以手动为其添加then方法)
     return (
         ((typeof value === 'object' && value !== null) ||
             typeof value === 'function') &&
@@ -14,6 +16,63 @@ function isPromise(value: any): value is PromiseLike<any> {
     )
 }
 
+/**
+ * then的链式调用处理函数,负责处理当前then方法返回值,传递给下一个then方法
+ *
+ */
+function resolvePromise<T>(
+    promise2: MyPromise<T>,
+    x: T | PromiseLike<T>,
+    resolve: Resolve<T>,
+    reject: Reject): any {
+    /**
+     * 判断x是否和当前promise是同一个对象,不然会造成无限循环
+     * TIPS: 当判断到是一个thenable对象时,会执行thenable对象的then方法,then方法又会返回一个相同的promise,如果不做判断,就对造成无限循环
+     * const promise2: any = new Promise((resolve) => {
+     *    resolve()
+     * }).then(res => promise2, err => {
+     *    console.error(err)   // TypeError: Chaining cycle detected for promise #<Promise>
+     * })
+     */
+    if (x === promise2) {
+        const e = new TypeError('TypeError: Chaining cycle detected for promise #<MyPromise>')
+        // 清空栈信息，防止出现多余的堆栈信息
+        e.stack = ''
+        // 直接进入错误的回调
+        return reject(e)
+    }
+    // 防止多次调用
+    let called = false
+
+    if ((typeof x === 'object' && x !== null) || typeof x === 'function') {
+        try {
+            const then = (x as PromiseLike<T>).then
+            if (typeof then === 'function') {
+                then.call(x, (y) => {
+                    if (called) return
+                    called = true
+                    // FIXME: 不理解?
+                    // 递归解析的过程（因为可能 promise 中还有 promise） Promise/A+ 2.3.3.3.1
+                    resolvePromise(promise2, y as T | PromiseLike<T>, resolve, reject)
+                }, (e) => {
+                    if (called) return
+                    called = true
+                    reject(e)
+                })
+            } else {
+                // 如果 then 是个普通值就直接返回 resolve 作为结果  Promise/A+ 2.3.3.4
+                resolve(x as T)
+            }
+        } catch (e) {
+            if (called) return
+            called = true
+            reject(e)
+        }
+    } else {
+        // 非thenable之外的普通值处理
+        reject(x)
+    }
+}
 
 class MyPromise<T> {
     // 开始状态的值
@@ -64,35 +123,56 @@ class MyPromise<T> {
         }
     }
 
-    public then<TResult1 = T, TResult2 = any>(
+    public then<TResult1 = T, TResult2 = never>(
         onFulfilled?: onFulfilled<T, TResult1>,
         onRejected?: onRejected<TResult2>
     ): MyPromise<TResult1 | TResult2> {
-        // 如果Promise状态是完成态,直接调用
-        if (this.status === Status.FULFILLED) {
-            // setTimeout 模拟异步微任务,实际上setTimeout是宏任务
-            setTimeout(() => {
-                onFulfilled!(this.value)
-            })
-        }
-        // 如果Promise状态是失败,直接调用
-        if (this.status === Status.REJECTED) {
-            // setTimeout 模拟异步微任务,实际上setTimeout是宏任务
-            setTimeout(() => {
-                onRejected!(this.reason)
-            })
-        }
-        // 如果Promise状态是pending,将成功和失败回调保存起来
-        if (this.status === Status.PENDING) {
-            this.onResolvedCallbacks.push(() => {
-                onFulfilled!(this.value)
-            })
-            this.onRejectedCallbacks.push(() => {
-                onRejected!(this.reason)
-            })
-        }
-        return new MyPromise(() => {
+        const promise2 = new MyPromise<TResult1 | TResult2>((resolve, reject) => {
+            // 如果Promise状态是完成态,直接调用
+            if (this.status === Status.FULFILLED) {
+                setTimeout(() => {
+                    // setTimeout 模拟异步微任务,实际上setTimeout是宏任务
+                    try {
+                        const x = onFulfilled!(this.value)
+                        resolvePromise(promise2, x, resolve, reject)
+                    } catch (e) {
+                        reject(e)
+                    }
+                })
+            }
+            // 如果Promise状态是失败,直接调用
+            if (this.status === Status.REJECTED) {
+                // setTimeout 模拟异步微任务,实际上setTimeout是宏任务
+                setTimeout(() => {
+                    try {
+                        const x = onRejected!(this.reason)
+                        resolvePromise(promise2, x, resolve, reject)
+                    } catch (e) {
+                        reject(e)
+                    }
+                })
+            }
+            // 如果Promise状态是pending,将成功和失败回调保存起来
+            if (this.status === Status.PENDING) {
+                this.onResolvedCallbacks.push(() => {
+                    try {
+                        const x = onFulfilled!(this.value)
+                        resolvePromise(promise2, x, resolve, reject)
+                    } catch (e) {
+                        reject(e)
+                    }
+                })
+                this.onRejectedCallbacks.push(() => {
+                    try {
+                        const x = onRejected!(this.reason)
+                        resolvePromise(promise2, x, resolve, reject)
+                    } catch (e) {
+                        reject(e)
+                    }
+                })
+            }
         })
+        return promise2
     }
 }
 
--- test/my-promise.test.ts
+++ test/my-promise.test.ts
@@ -1,14 +1,43 @@
 import MyPromise from '../src/MyPromise/index'
+//
+// const promise = new MyPromise<boolean>((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log('111');
+//         resolve(true)
+//     }, 2000);
+// })
+// console.log(12312);
+// let d1 = Date.now()
+// promise.then(res => {
+//     console.log(Date.now() - d1)
+//     console.log(res);
+// })
 
-const promise = new MyPromise<boolean>((resolve, reject) => {
-    setTimeout(() => {
-        console.log('111');
-        resolve(true)
-    }, 2000);
+//
+// const promise2: any = new Promise((resolve) => {
+//     // @ts-ignore
+//     resolve(1111)
+// }).then()
+
+// const promise2: Promise<any> = new Promise<void>((reslove) => {
+//     reslove()
+// }).then((y) => {
+//     return promise2
+// })
+
+// promise2.then(() => {}, console.log)
+
+new MyPromise<void>((resolve) => {
+    resolve()
+    console.log(123123)
 })
-console.log(12312);
-let d1 = Date.now()
-promise.then(res => {
-    console.log(Date.now() - d1)
-    console.log(res);
-})
+    .then(() => {
+        console.log('123123ffff')
+        return 'step1'
+    })
+    .then((res) => {
+        return res + ':' + 'step2'
+    })
+    .then((res) => {
+        console.log(res) // step1:step2
+    })
