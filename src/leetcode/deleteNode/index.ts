4ab2903cb6480eb7d1549a5616d5f44bea5ffc66
花开半亩地 <2572468699@qq.com>
2022-04-06 15:32:03 +0800

    feat: 完成4.6算法题 删除链表的倒数第 N 个节点

--- src/leetcode/deleteNode/index.ts
+++ src/leetcode/deleteNode/index.ts
@@ -1,3 +1,5 @@
+import { ListNode } from '../../DataStructuresAlgorithms/LinkedList/index'
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
@@ -9,30 +11,16 @@
  * @param {ListNode} node
  * @return {void} Do not return anything, modify node in-place instead.
  */
-class ListNode {
-	val: any
-	next: ListNode | null
-	constructor(val: any) {
-		this.val = val
-		this.next = null
-	}
-	setNext(node: ListNode) {
-		this.next = node
-		return node
-	}
-}
-
 var deleteNode = function (node: ListNode) {
 	node.val = node.next?.val
 	//然后删除下一个结点
 	node.next = node.next?.next || null
 }
 
-
 const testNode = new ListNode(5)
 const startNode = new ListNode(4)
 
 startNode.setNext(testNode).setNext(new ListNode(1)).setNext(new ListNode(9))
-console.log(startNode);
+console.log(startNode)
 deleteNode(testNode)
 console.log(startNode)
