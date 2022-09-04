f6db9a89c4a3f247bdd242aa32efaafdd33740a3
花开半亩地 <2572468699@qq.com>
2022-09-04 22:39:33 +0800

    feat: 并发执行支持同步函数

--- src/MaximumConcurrent/index.ts
+++ src/MaximumConcurrent/index.ts
@@ -1,6 +1,9 @@
-export type Task<T = any> = () => Promise<T>
-type Dispatch = (...task: Task[]) => void
-function createTaskDispatch(max = 5): Dispatch {
+export type AsyncTask<T = any> = () => Promise<T>
+export type SyncTask = (...arg: any[]) => any
+export type Task = AsyncTask | SyncTask
+export type Dispatch = (...task: Task[]) => void
+
+export function createTaskDispatch(max = 5): Dispatch {
 	const untouchedTasks: Task[] = []
 
 	const drainUntouchedTasks = debounce(() => {
@@ -14,9 +17,38 @@ function createTaskDispatch(max = 5): Dispatch {
 		}
 	})
 	return function dispatch<T extends Promise<T>>(...task: Task[]): void {
-		Array.prototype.push.apply(untouchedTasks, task)
+		const _task = task.map((taskItem) => {
+			if (!isPromise(task)) {
+				return () =>
+					new Promise<any>((resolve) => {
+						taskItem()
+						resolve(null)
+					})
+			}
+			return taskItem
+		})
+
+		Array.prototype.push.apply(untouchedTasks, _task)
 		drainUntouchedTasks()
 	}
+}
+
+function debounce(fn: (...arg: any[]) => any, delay = 16) {
+	let timer: NodeJS.Timeout | null = null
+	return (...arg: any[]) => {
+		if (timer) clearTimeout(timer)
+		timer = setTimeout(() => {
+			fn.apply(null, arg)
+		}, delay)
+	}
+}
+
+export function isDef(v: any): boolean {
+	return v !== undefined && v !== null
+}
+// 判断一个值是否是 promise
+function isPromise(val: any): boolean {
+	return isDef(val) && typeof val.then === 'function' && typeof val.catch === 'function'
 }
 
 const dispatch = createTaskDispatch(2)
@@ -31,15 +63,14 @@ const promise = (id: number) => {
 		})
 	}
 }
-
-dispatch(promise(1), promise(2), promise(3), promise(4))
-
-function debounce(fn: (...arg: any[]) => any, delay = 16) {
-	let timer: NodeJS.Timeout | null = null
-	return (...arg: any[]) => {
-		if (timer) clearTimeout(timer)
-		timer = setTimeout(() => {
-			fn.apply(null, arg)
-		}, delay)
-	}
-}
+dispatch(
+	promise(1),
+	function () {
+		console.log(123)
+	},
+	() => {
+		console.log(123456)
+	},
+	promise(3),
+	promise(4)
+)
